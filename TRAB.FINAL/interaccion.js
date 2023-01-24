aa1fc2d53c9ab345c40fb71b58ddc50749d0a5c0
CelidelRo <123425753+CelidelRo@users.noreply.github.com>
2023-01-23 23:17:48 -0300

    Add files via upload

---
+++ TRAB.FINAL/interaccion.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById('boton').onclick=function() {
+window.open('https://drive.google.com/drive/folders/1_rH875eiJlxaSBdhjwPQE7Wh5XcXTfOX?usp=sharing')
+}
+
+document.getElementById('mail').onclick=function() {
+window.open('https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=DmwnWtMlQgmbQjxQrHFsJMQQfrSblttdrFWXntgpJbvSMLTJqPmmgvGKpNfzgdpZKVnGXDCKWNDG')
+
+}
+
+document.getElementById('Celular').onclick=function() {
+    window.open('https://wa.me/542222222222?text=Hola,%20vi%20tu%20CV%20y%20me%20interesó%20ponerme%20en%20contacto.')
+    
+    }
